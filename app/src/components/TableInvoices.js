d2b744f724818f63ccadbe4276f4f6728b82edb5
Goomees <mateus.nicolau@devmagic.com.br>
2023-12-25 20:34:47 -0300

    feat(*): implemented fn of delete and download invoices and formated total price and compentecy date

--- app/src/components/TableInvoices.js
+++ app/src/components/TableInvoices.js
@@ -1,35 +1,57 @@
+import { formatarData } from "../utils/Date";
 import ButtonAction from "./ButtonComponent";
+
+import {formatCurrencyBRL} from '../utils/Currency'
 
 
 import './css/TableInvoices.css';
 
-export default function TableInvoices({ data }) {
-    return (
-      <div className="table-invoices">
-
+export default function TableInvoices({ data, fnDonwload, fnDelete }) {
+  return (
+    <div className="table-invoices">
         <table>
           <thead>
             <tr>
+              <td>ID</td>
               <td>Nº FATURA</td>
               <td>Nº CLIENTE</td>
-              <td>DATA</td>
+              <td>COMPETÊNCIA</td>
+              <td>VALOR</td>
               <td>AÇÃO</td>
             </tr>
           </thead>
           <tbody>
-            {data.map((item) => (
+            {data.length ? data.map((item) => (
               <tr key={item.numberInvoice}>
+                <td>{item.id ?? null}</td>
                 <td>{item.numberInvoice}</td>
                 <td>{item.numberClient}</td>
-                <td>{item.competency}</td>
+                <td>{formatarData(item.competency)}</td>
+                <td>{formatCurrencyBRL(item.totalPrice)}</td>
                 <td>
-                  <ButtonAction label="Baixar" type="sucess" size="small" />
-                  <ButtonAction label="Excluir" type="error" size="small" />
+                  <ButtonAction
+                    label="Baixar"
+                    onClick={() => fnDonwload(item)}
+                    type="sucess"
+                    size="small"
+                  />
+                  <ButtonAction
+                    label="Excluir"
+                    onClick={() => fnDelete(item)}
+                    type="error"
+                    size="small"
+                  />
                 </td>
               </tr>
-            ))}
+            )): null}
           </tbody>
         </table>
       </div>
-      );
+  )
+}
+
+
+
+function Table({ data }){
+  
 }
